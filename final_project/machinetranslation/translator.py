d1067a0bf99e8714244c065146e7596c7b9ee219
carlosgithu6 <carlos.besil@gmail.com>
2023-06-12 17:40:38 -0400

    Using translator

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,10 +3,11 @@ from deep_translator import MyMemoryTranslator
 
 def english_to_french(english_text):
     '''write the code here'''
-    english_text='Bonjour'
-    return english_text
+    translated = MyMemoryTranslator(source='english', target='french').translate(english_text)
+    return translated
 
 def french_to_english(french_text):
     '''write the code here'''
-    french_text = 'Hello'
-    return french_text
+    translated = MyMemoryTranslator(source='french', target='english').translate(french_text)
+  
+    return translated
